51965cf186bb74575dda9866239e525ed7e9c5b4
ParkJuhyun <wngus6429@gmail.com>
2021-06-21 23:28:50 +0900

    621
    621

---
+++ 강의자료/섹션1/1.index.js
@@ -0,0 +1,12 @@
+const min = document.querySelector(".확인");
+
+function solution(a, b, c) {
+  //먼저 2개를 비교하고 나머지 한개를 비교하는 방법
+  let answer;
+  if (a < b) answer = a;
+  else answer = b;
+  if (c < answer) answer = c;
+  return answer;
+}
+
+min.innerText = solution(44, 55, 33);
