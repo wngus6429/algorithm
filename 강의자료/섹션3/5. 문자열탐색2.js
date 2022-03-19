c37aa90d8e29ecefa238842db5aa3ea03134a87b
ParkJuhyun <wngus6429@gmail.com>
2022-03-19 20:08:12 +0900

    update 0319 algorism

---
+++ 강의자료/섹션3/5. 문자열탐색2.js
@@ -0,0 +1,20 @@
+const 문제 = "KKHSSSSSSSE";
+function solution(s) {
+  let answer = "";
+  let cnt = 1;
+  s = s + " "; //맨 마지막 비교를 위해
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1]) {
+      cnt++;
+    } else {
+      answer += s[i];
+      if (cnt > 1) {
+        answer += String(cnt);
+        cnt = 1;
+      }
+    }
+  }
+  return answer;
+}
+console.log(solution(문제));
+// 문자가
