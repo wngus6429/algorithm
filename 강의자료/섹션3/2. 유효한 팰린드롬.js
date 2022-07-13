491e4accdb17d5a1fa9680c7617b993b5b400a92
ParkJuhyun <wngus6429@gmail.com>
2022-07-13 18:59:20 +0900

    update

--- 강의자료/섹션3/2. 유효한 팰린드롬.js
+++ 강의자료/섹션3/2. 유효한 팰린드롬.js
@@ -1,10 +1,11 @@
-let problem = "found7, time: study; Yduts; emiu, 7Dnuof";
+let problem = 'found7, time: study; Yduts; emiu, 7Dnuof';
 function solution(s) {
-  let answer = "YES";
-  s = s.toLowerCase().replace(/[^a-z]/g, "");
+  let answer = 'YES';
+  s = s.toLowerCase().replace(/[^a-z]/g, '');
+  console.log(s);
   // 소문자 a부터 z까지 아닌것들을 찾아서 빈 문자열로
-  if (s.split("").reverse().join("") !== s) {
-    return "NO";
+  if (s.split('').reverse().join('') !== s) {
+    return 'NO';
   }
   return answer;
 }
--- 강의자료/섹션3/3. 숫자만 추출.js
+++ 강의자료/섹션3/3. 숫자만 추출.js
@@ -1,23 +1,27 @@
-// let word = "g0en2T0s8eSof02t";
-// function solution(word) {
-//   let answer = [];
-//   for (let i = 0; i < word.length; i++) {
-//     // for(let w of word)
-//     if (!isNaN(word[i])) {
-//       answer.push(word[i]);
-//     }
-//   }
-//   answer = answer.join("");
-//   return Number(answer); //parseInt
-// }
-// console.log(solution(word));
-
-let word = "g0en2T0s8eSof02t";
+let word = 'g0en2T0s8eSof02t';
 function solution(word) {
-  let answer = 0;
-  for (let x of word) {
-    if (!isNaN(x)) answer = answer * 10 + Number(x);
+  let answer = [];
+  for (let i = 0; i < word.length; i++) {
+    // for(let w of word)
+    if (!isNaN(word[i])) {
+      answer.push(word[i]);
+    }
   }
-  return answer;
+  console.log(answer);
+  answer = answer.join('');
+  return Number(answer); //parseInt
 }
 console.log(solution(word));
+
+// let word = 'g0en2T0s8eSof02t';
+// function solution(word) {
+//   let answer = 0;
+//   for (let x of word) {
+//     if (!isNaN(x)) {
+//       answer = answer * 10 + Number(x);
+//       console.log(answer);
+//     }
+//   }
+//   return answer;
+// }
+// console.log(solution(word));
