e11d8eb5264097158d42588ab0b4e87c23bcd762
ParkJuhyun <wngus6429@gmail.com>
2022-12-12 18:57:16 +0900

    Create 콜라.js

---
+++ 프로그래머스/콜라.js
@@ -0,0 +1,18 @@
+function solution(a, b, n) {
+    let answer = 0;
+    let 빈병수 = n;
+    while (true) {
+      if(a > 빈병수) {
+        break;
+      }
+      //빈병에서 교환갯수를 나눠서 받는 콜라 갯수 구함.
+      answer += parseInt(빈병수/a)*b;
+      // 남아 있는 빈병 갯수를 구함.
+      // 받는 콜라 갯수에 남아있는거까지 더함.
+      // 그리고 위에서 콜라 받는거 계산.
+      빈병수 = parseInt(빈병수/a)*b + 빈병수%a
+    }
+
+    return answer;
+}
+console.log(solution(2, 1, 20))
