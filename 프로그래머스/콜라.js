979b5d5bb32395fac635408c78880cf8d2d10534
ParkJuhyun <wngus6429@gmail.com>
2022-12-17 18:53:29 +0900

    Update 콜라.js

--- 프로그래머스/콜라.js
+++ 프로그래머스/콜라.js
@@ -6,7 +6,9 @@ function solution(a, b, n) {
         break;
       }
       //빈병에서 교환갯수를 나눠서 받는 콜라 갯수 구함.
+      console.log('빈병수', 빈병수)
       answer += parseInt(빈병수/a)*b;
+      console.log('콜라수', answer)
       // 남아 있는 빈병 갯수를 구함.
       // 받는 콜라 갯수에 남아있는거까지 더함.
       // 그리고 위에서 콜라 받는거 계산.
