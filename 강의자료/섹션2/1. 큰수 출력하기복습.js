f004c8b9377db4133a805f2e2efa298b2e657fac
Park Juhyun <wngus6429@gmail.com>
2024-11-22 03:48:03 +0900

    20241122

--- 강의자료/섹션2/1. 큰수 출력하기복습.js
+++ 강의자료/섹션2/1. 큰수 출력하기복습.js
@@ -1,15 +1,17 @@
 function solution(numbers) {
   let answer = [];
   answer.push(numbers[0]);
+  let temp = numbers[0];
   for (let i = 1; i < numbers.length; i++) {
-    let check = numbers[i - 1];
-    if (check < numbers[i]) {
+    if (numbers[i] > temp) {
       answer.push(numbers[i]);
     }
+    temp = numbers[i];
   }
   return answer;
 }
 
 const numbers = [7, 3, 9, 5, 6, 12];
 console.log(solution(numbers));
-// 20241005
+// 20241005 복습
+// 20241122 복습
--- 강의자료/섹션2/2. 보이는 학생(복습).js
+++ 강의자료/섹션2/2. 보이는 학생(복습).js
@@ -1,16 +1,17 @@
 function solution(numbers) {
   let answer = [];
   answer.push(numbers[0]);
-  let max = 0;
+  let big = numbers[0];
   for (let i = 1; i < numbers.length; i++) {
-    if (max < numbers[i]) {
+    if (numbers[i] > big) {
       answer.push(numbers[i]);
-      max = numbers[i];
+      big = numbers[i];
     }
   }
-  return answer;
+  return answer.length;
 }
 
 const numbers = [130, 135, 148, 140, 145, 150, 150, 153];
 console.log(solution(numbers));
 // 20241005 복습
+// 20241122 복습
