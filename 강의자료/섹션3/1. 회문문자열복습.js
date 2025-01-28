6e97c3b0284a540e77436de40e57c5e56ddb0386
Park Juhyun <wngus6429@gmail.com>
2025-01-29 04:01:54 +0900

    Update 1. 회문문자열복습.js

--- 강의자료/섹션3/1. 회문문자열복습.js
+++ 강의자료/섹션3/1. 회문문자열복습.js
@@ -1,10 +1,25 @@
 function solution2(word) {
-    if(word.toUpperCase() === word.toUpperCase().split('').reverse().join('')){
-        return "YES"
-    } else {
-        return "NO"
-    }
+  if (word.toUpperCase() === word.toUpperCase().split("").reverse().join("")) {
+    return "YES";
+  } else {
+    return "NO";
+  }
 }
 
-const problem2 = 'goooG';
+const problem2 = "goooG";
 console.log(solution2(problem2));
+// 20250129 복습
+
+function isPalindrome(word) {
+  word = word.toUpperCase();
+  const len = word.length;
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return "NO";
+    }
+  }
+  return "YES";
+}
+
+const problem3 = "racecar";
+console.log(isPalindrome(problem3));
