faa478f45d6eafe0c6eb4370ae280cf5d891510e
ParkJuhyun <wngus6429@gmail.com>
2025-02-02 19:16:21 +0900

    g

--- 강의자료/섹션3/2. 유효한 팰린드롬 복습.js
+++ 강의자료/섹션3/2. 유효한 팰린드롬 복습.js
@@ -1,7 +1,8 @@
 function solution(s) {
   let answer = "YES";
-  //! a-z 사이를 제외하고 모두 없던걸로 한다.
-  s = s.toLowerCase().replace(/[^a-z]/g, "");
+  //! a-z와 0-9 사이를 제외하고 모두 없던걸로 한다.
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  console.log(s);
   if (s.split("").reverse().join("") !== s) {
     return "NO";
   }
