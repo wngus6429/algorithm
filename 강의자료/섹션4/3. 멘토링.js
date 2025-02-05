fa342fdbd7279ffbef07f2e9d042b1689289a03b
Park Juhyun <wngus6429@gmail.com>
2025-02-06 03:57:14 +0900

    Update 3. 멘토링.js

--- 강의자료/섹션4/3. 멘토링.js
+++ 강의자료/섹션4/3. 멘토링.js
@@ -1,11 +1,11 @@
 // 블루투포스, 전체탐색
 function solution(test) {
-  let answer = 0; // 멘토멘티 횟수 초기화
+  let answer = 0;
   let tmp = [];
   m = test.length; // 시험 횟수
-  n = test[0].length; // 멘토의 시험 점수, 4가 되겟지
+  n = test[0].length; // 학생수
   // 멘토는 멘티 보다 성적이 좋아야 한다.
-  //인덱스인 i는 멘토가 되는 학생의 번호 (1부터 n번까지)
+  // 인덱스인 i는 멘토가 되는 학생의 번호 (1부터 n번까지)
   for (let i = 1; i <= n; i++) {
     // 인덱스인 j는 멘티가 되는 학생의 번호 (1부터 n번까지)
     for (let j = 1; j <= n; j++) {
@@ -23,6 +23,8 @@ function solution(test) {
           // if(test[k][s]===i) 이 참이되면 s는 i번 학생의 등수가 되는 것이고 그것을 pi에 저장하는 것입니다.
           // 따라서 pi는 멘토가 되는 i번 학생의 등수인 것입니다.
           // 그러면 pj는 멘티가 되는 j번 학생의 등수가 되겠죠.
+          console.log("i", i, "j", j);
+          console.log(test[k][s]);
           if (test[k][s] === i) pi = s;
           if (test[k][s] === j) pj = s;
         }
@@ -37,6 +39,8 @@ function solution(test) {
   console.log(tmp);
   return answer;
 }
+// 0번 인덱스가 보면 3번 학생이 1등
+// 3번 학생 1등, 4번학생 2등, 1번학생 3등, 2번학생 4등
 let arr = [
   [3, 4, 1, 2],
   [4, 3, 2, 1],
