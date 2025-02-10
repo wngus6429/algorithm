eeaa977c3fda652c7597a39849cabb054ae9cc05
Park Juhyun <wngus6429@gmail.com>
2025-02-11 02:52:41 +0900

    Update 5. 최대 매출.js

--- 강의자료/섹션5/5. 최대 매출.js
+++ 강의자료/섹션5/5. 최대 매출.js
@@ -7,6 +7,10 @@ function solution(k, arr) {
   answer = sum;
   for (let i = k; i < arr.length; i++) {
     // 한칸 앞으로 나갔으면 맨 뒤쪽도 빼줘야 하니까.
+    console.log(arr[i]);
+    // 한칸씩 큰숫자 더하고, 한칸씩 큰숫자 빼고
+    // arr[3] - arr[0] , arr[4] - arr[1], arr[5] - arr[2]
+    console.log("값", arr[i] - arr[i - k]);
     sum += arr[i] - arr[i - k]; // 3칸씩 앞으로 밀고감
     answer = Math.max(answer, sum);
   }
