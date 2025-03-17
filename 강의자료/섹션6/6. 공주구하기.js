985bf8dcb4a83aabe6b47ed25d2c51ef0180a8d8
Park Juhyun <wngus6429@gmail.com>
2025-03-18 01:00:31 +0900

    Update 6. 공주구하기.js

--- 강의자료/섹션6/6. 공주구하기.js
+++ 강의자료/섹션6/6. 공주구하기.js
@@ -1,15 +1,17 @@
 function solution(n, k) {
   let answer;
-  let queue = Array.from({ length: n }, (v, i) => i * 1);
-  console.log('본처', queue); // [ 0, 1, 2, 3, 4, 5, 6, 7 ]
+  let queue = Array.from({ length: n }, (v, i) => i + 1);
+  console.log("본처", queue); // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+  // queue 내용이 텅비면 멈추겟지
   while (queue.length) {
+    console.log("2개 뒤로 빼기전", queue);
     for (let i = 1; i < k; i++) {
       // queue에서 앞에서 2개를 차례로 뒤로 넣는다.
       queue.push(queue.shift());
     }
-    console.log('빼기전', queue);
+    console.log("2개 뒤로 뺀 후", queue);
     queue.shift(); // 한개 그냥 빼버림.
-    console.log('뺀후', queue);
+    console.log("3 외친놈 뺀후", queue);
     // 한개가 남으면 정답임.
     if (queue.length === 1) {
       answer = queue.shift();
@@ -17,5 +19,7 @@ function solution(n, k) {
   }
   return answer;
 }
-
-console.log('정답', solution(8, 3));
+// 1,2,3을 배열 인덱스 0부터 끝까지 계속 외치는데
+// 1,2 외친놈은 뒤로 넘기고, 3을 외친놈을 없애는거임
+console.log("정답", solution(8, 3));
+// 20250318 복습
