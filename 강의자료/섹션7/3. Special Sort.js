05f7f10df259c0f19356ea1638246928c9707488
Park Juhyun <wngus6429@gmail.com>
2025-04-04 03:26:50 +0900

    Update 3. Special Sort.js

--- 강의자료/섹션7/3. Special Sort.js
+++ 강의자료/섹션7/3. Special Sort.js
@@ -14,18 +14,20 @@ function solution(arr) {
 
 let arr = [1, 2, 3, -3, -2, 5, 6, -6];
 console.log(solution(arr));
+// 20250331 복습
 
 //! 음수들 먼저 push하고 그 다음에 양수 push방법
-// function solution(arr) {
-//   let answer = [];
-//   for (let x of arr) {
-//     if (x < 0) answer.push(x);
-//   }
-//   for (let x of arr) {
-//     if (x > 0) answer.push(x);
-//   }
-//   return answer;
-// }
+function solution2(arr) {
+  let answer = [];
+  for (let x of arr) {
+    if (x < 0) answer.push(x);
+  }
+  for (let x of arr) {
+    if (x > 0) answer.push(x);
+  }
+  return answer;
+}
 
-// let arr = [1, 2, 3, -3, -2, 5, 6, -6];
-// console.log(solution(arr));
+let arr2 = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution2(arr2));
+// 20250331 복습
